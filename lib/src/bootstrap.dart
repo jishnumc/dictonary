6eaafc879d68041ce8dda1eed1d3a92a77768634
jishnumc <jishnu.mc@applab.qa>
2026-05-05 18:28:53 +0530

    feat: implement ScaleUiBinding to provide responsive UI scaling across different screen sizes

--- lib/src/bootstrap.dart
+++ lib/src/bootstrap.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:dictonary/src/outer_layer/clients/storage_client.dart';
+import 'package:dictonary/src/system/scale_binding.dart';
 import 'package:dictonary/src/utils/app_provider_observer.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,21 +9,39 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<ProviderContainer> bootstrap() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  // Proportional scaling for different screen sizes
+  // This also initializes the WidgetsBinding
+  ScaleUiBinding.ensureInitialized(scaleResolver: _calculateScaleFactor);
+  //WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize essential dependencies
   final sharedPref = await SharedPreferences.getInstance();
 
   // Create ProviderContainer with overrides
-  final container = ProviderContainer(
-    overrides: [
-      sharedPreferencesProvider.overrideWithValue(sharedPref),
-    ],
-    observers: [const AppProviderObserver()],
-  )
-    // Read required providers so that they initialize correctly
-    ..read(storageClientProvider);
+  final container =
+      ProviderContainer(
+          overrides: [sharedPreferencesProvider.overrideWithValue(sharedPref)],
+          observers: [const AppProviderObserver()],
+        )
+        // Read required providers so that they initialize correctly
+        ..read(storageClientProvider);
   // ..read(settingsRepositoryProvider);
 
   return container;
+}
+
+/// Calculate the scale factor based on the screen size
+double _calculateScaleFactor(Size size) {
+  const double designWidth = 402;
+  const double designHeight = 874;
+
+  final widthScale = size.width / designWidth;
+  final heightScale = size.height / designHeight;
+
+  // Use min to ensure content fits.
+  // Clamp(0.75, 1.05):
+  // - 0.75: Supports iPhone SE (height constraint ~0.74) & small Androids.
+  // - 1.05: Prevents excessive zooming on large screens
+  // (Nothing Phone, S25 Ultra), keeping UI crisp.
+  return min(widthScale, heightScale).clamp(0.75, 1.05);
 }
